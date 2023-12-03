4f9439679df9e18b96ecf36037b35ef00fcfd885
Sergey Maldenberg <maldenberg@mail.ru>
2023-12-03 21:38:47 +0300

    style2

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,67 @@
-document
-  .getElementById("interview-form")
-  .addEventListener("submit", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("interview-form");
+  const submitButton = document.getElementById("submit-button");
+
+  form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // Получаем кнопку и добавляем лоадер
-    const submitButton = document.getElementById("submit-button");
-    submitButton.innerHTML = "Отправка...";
+    let isFormValid = true;
+
+    // Проверяем валидность каждого поля
+    Array.from(form.elements).forEach((input) => {
+      if (input.tagName === "BUTTON") return; // Пропускаем кнопку отправки
+      validateInput(input);
+      if (!input.checkValidity()) isFormValid = false;
+    });
+
+    if (!isFormValid) {
+      console.log("Форма заполнена некорректно");
+      return; // Прекращаем выполнение функции, если форма невалидна
+    }
+
+    // Если форма валидна, продолжаем отправку
+    submitButton.textContent = "Отправка...";
     submitButton.disabled = true;
 
-    // Сбор данных из формы
-    var formData = new FormData(document.getElementById("interview-form"));
+    const formData = new FormData(form);
 
-    // Опции для запроса
-    var requestOptions = {
+    const requestOptions = {
       method: "POST",
       body: formData,
       redirect: "follow",
     };
 
-    // Выполнение запроса к серверу
+    // Отправляем запрос на сервер
     fetch("/submit-interview", requestOptions)
       .then((response) => response.text())
       .then((result) => {
         console.log(result);
-        // Восстановление состояния кнопки после отправки
-        submitButton.innerHTML = "Отправить";
+        submitButton.textContent = "Отправить";
         submitButton.disabled = false;
       })
       .catch((error) => {
-        console.log("error", error);
-        // Восстановление состояния кнопки в случае ошибки
-        submitButton.innerHTML = "Отправить";
+        console.error("Ошибка:", error);
+        submitButton.textContent = "Отправить";
         submitButton.disabled = false;
       });
   });
+
+  // Валидация поля при потере фокуса
+  Array.from(form.elements).forEach((input) => {
+    input.addEventListener("blur", function () {
+      validateInput(this);
+    });
+  });
+
+  // Функция для валидации отдельного поля
+  function validateInput(input) {
+    let errorMessage = input.nextElementSibling;
+    if (!input.validity.valid) {
+      errorMessage.style.display = "block";
+      input.classList.add("invalid");
+    } else {
+      errorMessage.style.display = "none";
+      input.classList.remove("invalid");
+    }
+  }
+});
