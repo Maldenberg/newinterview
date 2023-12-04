a367246bb247e796ad9465e99e8a4410c609207d
Sergey Maldenberg <maldenberg@mail.ru>
2023-12-04 09:58:21 +0300

    стилизовал форму

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("interview-form");
+  const submitButton = form.querySelector('button[type="submit"]'); // добавили эту строку
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    // Отключаем валидацию формы
-    // let isFormValid = true;
-
-    // Array.from(form.elements).forEach((input) => {
-    //   if (input.tagName === "BUTTON") return; // Пропускаем кнопку отправки
-    //   // validateInput(input); // Теперь не вызываем validateInput
-    //   if (!input.checkValidity()) isFormValid = false;
-    // });
-
-    // if (!isFormValid) {
-    //   console.log("Форма заполнена некорректно");
-    //   return; // Прекращаем выполнение функции, если форма невалидна
-    // }
-
-    // Если форма валидна, продолжаем отправку
-    // Эти строки можно раскомментировать, если они не вызывают проблем
-    // submitButton.textContent = "Отправка...";
-    // submitButton.disabled = true;
+    // Теперь мы можем управлять кнопкой "Отправить"
+    submitButton.textContent = "Отправка...";
+    submitButton.disabled = true;
 
     const formData = new FormData(form);
 
@@ -36,34 +22,13 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((response) => response.text())
       .then((result) => {
         console.log(result);
-        // Эти строки можно раскомментировать, если они не вызывают проблем
-        // submitButton.textContent = "Отправить";
-        // submitButton.disabled = false;
+        submitButton.textContent = "Отправить";
+        submitButton.disabled = false;
       })
       .catch((error) => {
         console.error("Ошибка:", error);
-        // Эти строки можно раскомментировать, если они не вызывают проблем
-        // submitButton.textContent = "Отправить";
-        // submitButton.disabled = false;
+        submitButton.textContent = "Отправить";
+        submitButton.disabled = false;
       });
   });
-
-  // Убираем валидацию на потерю фокуса
-  // Array.from(form.elements).forEach((input) => {
-  //   input.addEventListener("blur", function () {
-  //     // validateInput(this); // Теперь не вызываем validateInput
-  //   });
-  // });
-
-  // Убираем функцию validateInput
-  // function validateInput(input) {
-  //   let errorMessage = input.nextElementSibling;
-  //   if (!input.validity.valid) {
-  //     errorMessage.style.display = "block";
-  //     input.classList.add("invalid");
-  //   } else {
-  //     errorMessage.style.display = "none";
-  //     input.classList.remove("invalid");
-  //   }
-  // }
 });
